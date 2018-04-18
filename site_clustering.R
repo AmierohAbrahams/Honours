fe1be5432a8819ad12eb4880c1f7741e5b54a737
AJ Smit <albertus.smit@gmail.com>
2018-04-18 20:56:05 +0200

    Some edits and suggestions.

--- site_clustering.R
+++ site_clustering.R
@@ -6,9 +6,9 @@
 
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
-library(FNN)
+library(ggpubr)
 library(zoo)
-
+library(lubridate)
 
 # Load data ---------------------------------------------------------------
 
@@ -25,14 +25,14 @@ kmeans(site_list[,c(15, 18, 19)], 3)$cluster
 
 clust_columns <- site_list[,c(15:19)]
 
-clust_i <- function(i){
+clust_i <- function(i) {
   ggplot(data = site_list, 
                   aes(x = lon, y = lat, 
                       colour = as.factor(kmeans(clust_columns, i)$cluster))) +
   borders() +
   geom_point() +
   labs(colour = "cluster") +
-  coord_cartesian(xlim = c(15, 35), ylim = c(-37, -27)) +
+  coord_equal(xlim = c(15, 35), ylim = c(-37, -27)) +
   ggtitle(paste0("clust = ", i))
 }
 
@@ -46,45 +46,73 @@ ggarrange(clust_6, clust_5, clust_4, clust_3, common.legend = T)
 
 # Cluster sites by k-means results ----------------------------------------
 
-load("SACTN_daily_v4.2.RData")
+load("SACTN_data/SACTN_daily_v4.2.RData")
 
 # Create cluster index
 site_list$cluster <- as.factor(kmeans(site_list[,c(15, 18:19)], 4)$cluster)
 
 SACTN_daily_clusters <- left_join(SACTN_daily_v4.2, site_list[,c(4, 13, 21)]) %>% 
-  filter(length >= 3650)
+  filter(length >= 3650 * 3) 
+# Here adjusted to 30 years, which works with the KZNSB data;
+# use a shorter time for sites along other coastlines
 
 SACTN_clust_1 <- SACTN_daily_clusters %>% 
   filter(cluster == 1) %>% 
-  select(-length)
-SACTN_clust_1 <- droplevels(SACTN_clust_1 )
+  select(-length) %>% 
+  droplevels()
 
-SACTN_clust_1_match <- data.frame()
-# for(i in 1:length(levels(SACTN_clust_1$index))){
-for(i in 1:4){
-  SACTN_df_1 <- filter(SACTN_clust_1, index == levels(index)[i])
-  # for(j in 1:length(levels(SACTN_clust_1$index))){
-  for(j in 1:4){
-    if(i == j){
-      next
+# How many sites in this cluster?
+length(unique(SACTN_clust_1$index)) 
+# 42 of them if time series are 10 years or less; 
+# or 33 if lengths of 30 years are selected...
+# This might have to be adjusted depending on the coastline 
+# we are working on
+
+# This code below takes only a few minutes on my computer (AJS)...
+# I now also wrapped it in an argument-less function...
+match_sites <- function() {
+  SACTN_clust_1_match <- data.frame()
+  for (i in 1:length(levels(SACTN_clust_1$index))) {
+    # for (i in 1:10) {
+    SACTN_df_1 <- filter(SACTN_clust_1, index == levels(index)[i])
+    for (j in 1:length(levels(SACTN_clust_1$index))) {
+      # for(j in 1:10) {
+      if (i == j) {
+        next
+      }
+      if (j < i) {
+        next
+      }
+      SACTN_df_2 <- filter(SACTN_clust_1, index == levels(index)[j])
+      SACTN_df_3 <-
+        left_join(SACTN_df_1, SACTN_df_2, by = "date") %>%
+        na.trim() %>%
+        select(date,
+               index.x,
+               temp.x,
+               index.y,
+               temp.y,
+               -cluster.x,
+               -cluster.y) %>%
+        rename(
+          index_1 = index.x,
+          temp_1 = temp.x,
+          index_2 = index.y,
+          temp_2 = temp.y
+        ) %>%
+        mutate(index_pair = paste0(index_1, " - ", index_2))
+      SACTN_clust_1_match <- rbind(SACTN_clust_1_match, SACTN_df_3)
     }
-    if(j < i){
-      next
-    }
-    SACTN_df_2 <- filter(SACTN_clust_1, index == levels(index)[j])
-    SACTN_df_3 <- left_join(SACTN_df_1, SACTN_df_2, by = "date") %>% 
-      na.trim() %>% 
-      select(date, index.x, temp.x,
-             index.y, temp.y,
-             -cluster.x, -cluster.y) %>% 
-      rename(index_1 = index.x, temp_1 = temp.x,
-             index_2 = index.y, temp_2 = temp.y) %>% 
-      mutate(index_pair = paste0(index_1, " - ", index_2))
-    SACTN_clust_1_match <- rbind(SACTN_clust_1_match, SACTN_df_3)
   }
 }
 
-
+# Run the function, and also time it for interest sake...
+system.time(match_sites())
+# On my computer (AJS)... time in seconds
+# > system.time(match_sites())
+# user  system elapsed 
+# 394.857 145.019 541.344 
+# this is about 9 minutes
 
 # Create paired legitimate difference values  -----------------------------
 
@@ -100,17 +128,29 @@ SACTN_clust_1_legit <- SACTN_clust_1_match %>%
 # Visualise results -------------------------------------------------------
 
 # Paired values
+
 ggplot(data = SACTN_clust_1_match, aes(x = index_pair)) +
   geom_boxplot(aes(y = temp_1), fill = "khaki4", alpha = 0.3) +
   geom_boxplot(aes(y = temp_2), fill = "chartreuse3", alpha = 0.3) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Legit mean values
-ggplot(data = SACTN_clust_1_legit, 
-       aes(x = year, y = temp_mean_month_year)) +
-  geom_line(aes(group = index_pair, colour = index_pair), alpha = 0.7) +
-  geom_smooth(method = "gam", se = F, aes(colour = index_pair))  +
-  facet_wrap(~month)
+# I plot only the sites that are paired with "Glenmore/KZNSB -"
+# We need the stringr package for that
+# Also, for the KZNSB data we have time series of >30 years, and these are the ones
+# plotted here...
+library(stringr)
+SACTN_clust_1_legit %>% 
+  filter(str_detect(index_pair, "Glenmore/KZNSB -")) %>% 
+  ggplot(aes(x = year, y = temp_mean_month_year)) +
+  geom_line(aes(group = index_pair, colour = index_pair), alpha = 0.7, show.legend = F) +
+  geom_smooth(method = "gam", se = F, aes(colour = index_pair), show.legend = F)  +
+  facet_wrap(~month) + 
+  theme_pubclean()
+# Referring to the plot above: since the KZNSB data series are very long, we can
+# have time series of 30 years in length;
+# I'd also calculate a column with the distances between site pairs, and then
+# colour the lines as a function of the site-site distances in this column
 
 # Legit SD values
 ggplot(data = SACTN_clust_1_legit, 
